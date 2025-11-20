179cf1ecafaa15eae8ea413d24a3e8e1f520f73a
abrnsyh <abriansyah1099@gmail.com>
2025-11-20 21:20:16 +0700

    Add view detail button at member list

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -54,7 +54,18 @@
                                     <td>{{ $member->email }}</td>
                                     <td>{{ $member->phone }}</td>
                                     <td>{{ Number::currency($member->balance, 'IDR', 'id') }}</td>
-                                    <td class="text-right"></td>
+                                    <td>
+                                        <button class="btn-sm-icon-outline">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="lucide lucide-eye-icon lucide-eye">
+                                                <path
+                                                    d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0" />
+                                                <circle cx="12" cy="12" r="3" />
+                                            </svg>
+                                        </button>
+                                    </td>
                                 </tr>
                             @empty
                                 <tr>
